a2e6781fd5cadcf3613a23b8adbc9fa23ff0c0c2
junwo <loust3598@naver.com>
2021-09-22 18:15:53 +0900

    에러 발생시 경로변경 작업중

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -14,22 +14,64 @@ def columnGenerator(rows,maxColumn):
     return columns
 
 
+def rowCheck(row,number):        
+    result = sorted(list(set(number) - set(row)))                
+    return result
+    
+
+def colCheck(col,number):        
+    result = sorted(list(set(number) - set(col)))
+    return result
+    
+def current_possible_row_check(rows,possible_number,row_index,current_row_index):
+    print(rows[f"row{row_index}"],str(possible_number).ljust(30),f"{row_index}행, {current_row_index+1}열")
+
+def tableCheck(rows):
+    print("="*14+"table"+"="*14)
+    for i in range(9):
+        print(f"row{i+1}",rows[f"row{i+1}"])
+    print("="*15+"end"+"="*15)
+
 def backtrackingAlgorithm(dataList):
     dataList = [int(x) for x in dataList]
     rows = rowGenerator(dataList,9)
     columns = columnGenerator(rows,9)
-    number = tuple(x for x in range(1,10))    
+    number = list(x for x in range(1,10))    
         
-    for tableIndex in range(1,10):        
-        for i in range(len(number)):
-            if number[i] in rows[f"row{tableIndex}"]:            
-                continue
-            else:
-                if number[i] in columns[f"col{tableIndex}"]:                
-                    continue
-                else:
-                    """
-                    다 성공하는 경우 number를 배열에 넣어야 함
-                    set list ? 둘중 어느 것이 더 빠른가?
-                    """
+    possible_number_list = []
+    error_cnt = 0
+    for row_index in range(1,10):
+        loop = True        
+        iop = 0 # pi: index of possible_number
+        while loop:
+            if 0 in rows[f"row{row_index}"]:
+                row_number = rowCheck(rows[f"row{row_index}"],number)                
+                current_row_index = rows[f"row{row_index}"].index(0)                
+                possible_number = colCheck(columns[f"col{current_row_index+1}"],row_number)     
+                try:
+                    possible_number_list.append(possible_number)                    
+                    # rows[f"row{row_index}"][current_row_index] = possible_number[iop]
+                    # columns[f"col{current_row_index+1}"][row_index-1] = possible_number[iop]
+                    rows[f"row{row_index}"][current_row_index] = possible_number_list[-1][iop]
+                    columns[f"col{current_row_index+1}"][row_index-1] = possible_number_list[-1][iop]
+                    current_possible_row_check(rows,possible_number,row_index,current_row_index)
+                    iop = 0                    
+                
+                except IndexError:  
+                    error_cnt += 1
+                    print("error 발생 횟수",error_cnt)                         
+                    print(possible_number_list[-2][iop])                                     
+                    value = possible_number_list[-2][iop]
+                    search_index = rows[f"row{row_index}"].index(value)
+                    # search_index = rows[f"row{row_index}"].index(3)                    
+                    rows[f"row{row_index}"][search_index] = 0
+                    iop += 1                    
+                    tableCheck(rows)
+                    # print("test",rows[f"row{row_index}"])                   
+                    # raise IndexError
                     pass
+                           
+            else:
+                loop = False
+    tableCheck(rows)           
+    
